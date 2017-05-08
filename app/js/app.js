d43a2fedf0b51422bd291084f3064946f5e0a309
陈敬 <chenjing@physicalpoint.com>
2017-05-08 10:08:11 +0800

    demo修复

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
  * Created by ArvinChen9539 on 2017/5/4.
  */
 var app = angular
-    .module('app', ['echartsUI']);
+    .module('app', ['ng-echarts-select']);
 
 app.config(['$echartsOptionsProvider',function($echartsOptionsProvider){
     //$echartsOptionsProvider.theme = "test";
