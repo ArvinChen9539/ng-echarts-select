3c574aa9312d87ab820b44055cbaf8f7936a5e28
陈敬 <chenjing@physicalpoint.com>
2017-05-08 09:46:16 +0800

    模板放在demo中

--- app/js/app.js
+++ app/js/app.js
@@ -10,3 +10,32 @@ app.config(['$echartsOptionsProvider',function($echartsOptionsProvider){
      $echartsOptionsProvider.markWidth = '5';*/
 
 }]);
+
+/**
+ * 新建图表指令模板
+ */
+app.directive('exampleChart', ['$echartsOptions', function ($echartsOptions) {
+    return $.extend(true, {
+        link: function (scope, ele, attrs, parent) {
+            //=============p_参数说明及备注==========================
+            //p_noExtend 是否组合父指令提供的必要参数 delft:false(组合)
+            //p_noSeriesE 是否组合Series默认参数 delft:false(组合)
+            //p_noSeriesItemE 是否组合默认seriesItem参数 delft:false(组合)
+            //p_noDataE 是否组合标准data配置 delft:false(组合) 点击标记的重要参数
+            //p_noClickMark 是否处理点击标记逻辑控制 delft:false(标记)
+            /**
+             * 数据及事件处理
+             * @param data
+             */
+            scope.prepareChart = function (data) {
+                //图标数据配置模板
+                scope.option = {
+                    //图表模板
+                };
+                //data是页面上ngModel的对象 把数据设置到模板option里
+            };
+            //初始化图表并创建监听事件
+            parent.initChart(ele, scope);
+        }
+    }, $echartsOptions.chartConfig);
+}]);
--- app/src/js/ng-echarts-select.js
+++ app/src/js/ng-echarts-select.js
@@ -479,34 +479,6 @@
         };
     }]);
 //==============================================共用指令str=====================================================
-    /**
-     * 新建指令模板
-     */
-    app.directive('exampleChart', ['$rootScope', function ($rootScope) {
-        return $.extend(true, {
-            link: function (scope, ele, attrs, parent) {
-                //=============p_参数说明及备注==========================
-                //p_noExtend 是否组合父指令提供的必要参数 delft:false(组合)
-                //p_noSeriesE 是否组合Series默认参数 delft:false(组合)
-                //p_noSeriesItemE 是否组合默认seriesItem参数 delft:false(组合)
-                //p_noDataE 是否组合标准data配置 delft:false(组合) 点击标记的重要参数
-                //p_noClickMark 是否处理点击标记逻辑控制 delft:false(标记)
-                /**
-                 * 数据及事件处理
-                 * @param data
-                 */
-                scope.prepareChart = function (data) {
-                    //图标数据配置模板
-                    scope.option = {
-                        //图表模板
-                    };
-                    //data是页面上ngModel的对象 把数据设置到模板option里
-                };
-                //初始化图表并创建监听事件
-                parent.initChart(ele, scope);
-            }
-        }, $echartsOptions.chartConfig);
-    }]);
 
     /**
      * 图表共用ctrl
